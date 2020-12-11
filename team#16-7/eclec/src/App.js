297ac84ee756b2c99981cfb8fcbbc1ca4f38bca2
Naman-1234 <namankalrabhiwani54@gmail.com>
2020-12-11 18:52:35 +0530

    For getting logo

--- team#16-7/eclec/src/App.js
+++ team#16-7/eclec/src/App.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import React from "react";
 import "./components/Registration/Registration";
 import Registration from "./components/Registration/Registration";
@@ -8,24 +6,6 @@ function App() {
   return (
     <div className="App">
     <Registration />
-=======
-import React from "react"
-import Home from "./components/home"
-import {BrowserRouter,Route,Switch} from 'react-router-dom'
-// import Login from "./components/Sign-in/"
-function App() {
-  return (
-    <div className="App">
-     <BrowserRouter>
-            <div>
-               <Switch>
-                   <Route exact path='/' component={Home} />
-                   {/* <Route path='/login' component={Login} />   
-                   <Route path='/signup' component={Signup} /> */}
-               </Switch>
-            </div>
-        </BrowserRouter>
->>>>>>> d7a005d0da1fb58ef354f191498afa838847b3d8
     </div>
   );
 }
--- team#16-7/eclec/src/components/Registration/Registration.js
+++ team#16-7/eclec/src/components/Registration/Registration.js
@@ -1,49 +1,75 @@
-import React from "react";
+import React,{useState} from "react";
 // import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./registration.css";
-
 function Registration()
 {
+    const obj={
+        fname:"",
+        lname:"",
+        email:"",
+        password:"",
+        address:"",
+        countrychoose:"",
+        statechoose:"",
+        zipcode:"",
+    };
+    const [state,handlestate]=useState(obj);
+    function handleChange(event)
+    {
+        const value=event.target.value;
+        const name=event.target.name;
+     handlestate(
+         (prevalue)=>
+         {
+             return {...prevalue,[name]:value};
+         }
+     )  
+    }
+    function handleSubmit()
+    {
+        console.log(state);
+    }
 return <>
-    <h1 className="heading">Welcome to Ereca</h1>
+    <h1 className="heading">Welcome to Eclec</h1>
     <div className="row">
-        <input type="text" name="fname" placeholder="Enter First Name"/>
+        <input type="text" name="fname" placeholder="Enter First Name" value={state.fname} onChange={handleChange}/>
     </div>
     <div className="row">
-        <input type="text" name="lname" placeholder="Enter Last Name"></input>
+        <input type="text" name="lname" placeholder="Enter Last Name" value={state.lname}onChange={handleChange}></input>
     </div>
     <div className="row">
-        <input type="email" name="email" placeholder="Enter Email"></input>
+        <input type="email" name="email" placeholder="Enter Email" value={state.email}onChange={handleChange}></input>
     </div>
     <div className="row">
-        <input type="password" name="password" placeholder="Enter your password">
+        <input type="password" name="password" placeholder="Enter your password" value={state.password}onChange={handleChange}>
         </input>
     </div>
     <div className="row">
-        <input type="text" name="address" placeholder="Enter your address">
+        <input type="text" name="address" placeholder="Enter your address" value={state.address}onChange={handleChange}>
         </input>
     </div>
     <div className="row1">
     <div >
     <label for="country">country</label><br></br>
-       <select id="country" name="countrychoose" required>
-           <option value="">Choose...</option>
-           <option>India</option>
+       <select id="country" name="countrychoose" required onChange={handleChange}>
+           <option>Choose...</option>
+           <option value="India">India</option>
        </select>
     </div>
     <div >
     <label for="state">State</label><br></br>
        <select id="state" name="statechoose"required>
-           <option value="">Choose...</option>
-           <option>Haryana</option>
+           <option value="Choose...">Choose...</option>
+           <option value="Haryana">Haryana</option>
        </select>
     </div>
     <div >
         <label for="zip">Zip Code</label><br></br>
-        <textarea id="zip" rows="1" cols="6" name="zipcode" required>
+        <textarea id="zip" rows="1" cols="6" name="zipcode" required value={state.zipcode}onChange={handleChange}>
         </textarea> 
     </div>
     </div>
+    <button type="submit" onClick={handleSubmit} className="btn">Submit</button>
     
 </>
 }
