a3b0d8fa13279389cfe5def4c8b7b53f01c06fcb
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-12-11 17:03:17 +0530

    initial first page made

--- team#33 - 3/clinico/lib/services/auth.dart
+++ team#33 - 3/clinico/lib/services/auth.dart
@@ -1,22 +1,14 @@
 import 'package:clinico/model/user.dart';
-import 'package:clinico/pages/signin/signinbutton.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthServices {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = new GoogleSignIn();
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
-  String status;
-  AuthServices({this.status});
-
-
 
   MyUser _userFromFirebase(User user){
-
-      return user!=null?MyUser(uid:user.uid,photoURL: user.photoURL,displayName: user.displayName,email: user.email,status: status):null;
+      return user!=null?MyUser(uid:user.uid,photoURL: user.photoURL,displayName: user.displayName,email: user.email):null;
   }
 
   //stream for auth state change
