0b2c6da0531c45b9be6dcfd839e7eaffeb404735
BitAAM <bitaamdeveloper@gmail.com>
2020-12-11 11:54:37 +0530

    h

--- team#23-5/EduFree/app/src/main/java/edu/free/MainActivity.java
+++ team#23-5/EduFree/app/src/main/java/edu/free/MainActivity.java
@@ -1,11 +1,30 @@
 package edu.free;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    EditText userName,userPassword;
+    TextView signInToUp;
+    Button signIn;
 
 
     @Override
@@ -13,6 +32,100 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progBar);
+        userName = findViewById(R.id.email);
+        userName.requestFocus();
+        userPassword = findViewById(R.id.userP);
+
+        signInToUp = findViewById(R.id.signInToUp);
+        signIn = findViewById(R.id.LoginUser);
+
+        signInToUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SignNewUserActivity.class));
+                finish();
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+        });
+
+
 
     }
+
+    private void userLogin(){
+
+        String useremail = userName.getText().toString().trim();
+        String userpassword = userPassword.getText().toString().trim();
+
+        if(useremail.isEmpty()){
+
+            userName.setError("Email is Empty");
+            userName.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(useremail).matches()){
+            userName.setError("Please enter valid email");
+            userName.requestFocus();
+            return;
+        }
+
+        if(userpassword.isEmpty()){
+
+            userPassword.setError("Password is Empty");
+            userPassword.requestFocus();
+            return;
+        }
+
+        if(userpassword.length()<6){
+            userPassword.setError("Minimum length is 6");
+            userPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(useremail,userpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    progressBar.setVisibility(View.GONE);
+                    Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    userName.requestFocus();
+                    progressBar.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser()!=null){
+            finish();
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+        }
+
+    }
+
+
 }
---
+++ team#23-5/EduFree/app/src/main/java/edu/free/SignNewUserActivity.java
@@ -0,0 +1,14 @@
+package edu.free;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignNewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_new_user);
+    }
+}
