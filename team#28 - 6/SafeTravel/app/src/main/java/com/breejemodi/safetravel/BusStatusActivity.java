00a27c39812d4e0b3b7cbfca3c6e3d693cdb5974
Chaitanya-Soni <201951046@iiitvadodara.ac.in>
2020-12-11 20:31:07 +0530

    Add team#28

---
+++ team#28 - 6/SafeTravel/app/src/main/java/com/breejemodi/safetravel/BusStatusActivity.java
@@ -0,0 +1,14 @@
+package com.breejemodi.safetravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_status);
+    }
+}
