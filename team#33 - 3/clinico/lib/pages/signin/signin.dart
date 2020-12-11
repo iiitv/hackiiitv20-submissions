685795685fd2bcabe03b9692ac1ae654f4caa0f3
Ayush Patel <in.ayushpatel@gmail.com>
2020-12-11 16:27:05 +0530

    sign in page ready

--- team#33 - 3/clinico/lib/pages/signin/signin.dart
+++ team#33 - 3/clinico/lib/pages/signin/signin.dart
@@ -8,20 +8,44 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(height: 40,),
-            SignButton(name: "Sign in as Paitent "),
-            SizedBox(height: 40,),
-            SignButton(name: "Sign in as Doctor "),
-          ],
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height*.5,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: const Radius.circular(40),
+                          bottomRight: const Radius.circular(40),
+                        )
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      child: Image.asset('assets/signin.png', height: 300,),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40,),
+              Center(
+                child: SignButton(name: "Sign In as Paitent "),
+              ),
+              SizedBox(height: 40,),
+              Center(
+                child: SignButton(name: "Sign in as Doctor "),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+  }}
