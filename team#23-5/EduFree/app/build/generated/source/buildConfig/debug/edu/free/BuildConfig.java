bc1d248291163474e2495ed81d1f1594a492b254
BitAAM <bitaamdeveloper@gmail.com>
2020-12-11 12:30:33 +0530

    h

---
+++ team#23-5/EduFree/app/build/generated/source/buildConfig/debug/edu/free/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.free;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.free";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
