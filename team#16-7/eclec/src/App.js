d7a005d0da1fb58ef354f191498afa838847b3d8
Creator5923 <nlokhande5923@gmail.com>
2020-12-11 15:56:21 +0530

    front page half work done

--- team#16-7/eclec/src/App.js
+++ team#16-7/eclec/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import Home from "./components/home"
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+// import Login from "./components/Sign-in/"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+            <div>
+               <Switch>
+                   <Route exact path='/' component={Home} />
+                   {/* <Route path='/login' component={Login} />   
+                   <Route path='/signup' component={Signup} /> */}
+               </Switch>
+            </div>
+        </BrowserRouter>
     </div>
   );
 }
