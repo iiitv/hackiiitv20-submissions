d1585cb2cf7fec2109a64c2546e1d14626dd7bc1
MohitPanchasara <56157214+MohitPanchasara@users.noreply.github.com>
2020-12-11 22:41:49 +0530

    Create HealthCare.java

---
+++ team#15 - {3}/HealthCare.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
