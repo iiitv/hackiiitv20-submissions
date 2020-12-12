da35c112cb6df40e1e422bbb277c8f83b5366388
Ashish Singh <ashish.k.singh1001@gmail.com>
2020-12-12 14:10:08 +0530

    Minor fixes

--- team#21-{3}/js/main.js
+++ team#21-{3}/js/main.js
@@ -1,6 +1,6 @@
 var symid, gender, dob, i = 0;
 
-var authtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1lY2lyMjczMDJAeWVrdGFyYS5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU2NjkiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIxMDkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiMTAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiQmFzaWMiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTEyLTExIiwiaXNzIjoiaHR0cHM6Ly9hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjA3NzI4Njg5LCJuYmYiOjE2MDc3MjE0ODl9.w62c2YvV49Suor_0xIzi2h5tVF8rFZk89CttEdtCH2E";
+var authtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImdhbWVyYWtzMzM2NjUyQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNTY2NyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjEwOSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiIxMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJCYXNpYyIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjAtMTItMTEiLCJpc3MiOiJodHRwczovL2F1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE2MDc3Njg1NjcsIm5iZiI6MTYwNzc2MTM2N30.6Q-7WlEobHGrwK-4aVoZA73c_i93A92Vj1C-tzxXM2Q";
 
 const api1 = `https://healthservice.priaid.ch/symptoms?token=${authtoken}&format=json&language=en-gb`;
 fetch(api1)
@@ -26,7 +26,7 @@ function find() {
     }
 
     for (i = 0; i < 281; i++) {
-        if (symt.localeCompare(symdata[i].Name)) {
+        if (symt == symdata[i].Name) {
             symid = symdata[i].ID;
         }
     }
@@ -40,7 +40,7 @@ function find() {
             console.log(data);
 
             document.getElementById('soln').innerHTML = data.map(sol =>
-                `<div class="news">
+                `<div class="sympData">
                 
                     <h2 class="heading-secondary no-bg" id="icdname">
                         ${sol.Issue.IcdName}
@@ -49,7 +49,7 @@ function find() {
                         ${sol.Issue.Name}
                     </p>
                     <br>
-                    <h2 class="heading-secondary">Solution</h2>
+                    <h2 class="heading-secondary no-bg">Solution</h2>
                     ${sol.Specialisation.map(spe =>
                     `<li>${spe.Name}</li>`
                 ).join('')}
