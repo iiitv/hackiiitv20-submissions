88a139824f3c43fd3b66f302bcea907108fb4aee
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2020-12-12 12:01:44 +0530

    state fixed

--- team1-3/src/components/Questionnaire/Questionnaire.js
+++ team1-3/src/components/Questionnaire/Questionnaire.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Form,Button } from 'react-bootstrap'
 
 //This questionnaire is for the patient to fill and doctors to detect
 class Questionnaire extends Component {
@@ -6,7 +7,7 @@ class Questionnaire extends Component {
         super(props);
         this.state = {
           problem: '',
-          age: null,
+          age: '',
           symptoms: '',
           previous: '',
         };
@@ -16,37 +17,76 @@ class Questionnaire extends Component {
         let val = event.target.value;
         let symptom = event.target.symptoms;
         let previous = event.target.previous;
-        this.setState({[prob]: val});  //add other variables here
+        this.setState({[prob]: val});  
+    }
+
+    onSubmitHandler = (object) => {
+      this.setState({
+        problem:object.problem,
+        age:object.age,
+        symptoms:object.symptoms,
+        previous:object.previous
+      })
     }
     render() {
+
+      let obj = {
+          problem: '',
+          age: '',
+          symptoms: '',
+          previous: '',
+      }
+
         return (
-        <form>
+        <form onSubmit={()=>this.onSubmitHandler(obj)}>
+
+        
         <p>Enter your age:</p>
         <input
           type='text'
           name='age'
-          onChange={this.myChangeHandler}
+          onChange={(e)=> obj.age = e.target.value}
         />
         <p>Enter your symptoms:</p>
         <input
           type='text'
           name='symptoms'
-          onChange={this.myChangeHandler}
+          onChange={(e)=> obj.symptoms = e.target.value}
         />
         <p>Desciption of your problem:</p>
         <input
           type='text'
           name='problem'
-          onChange={this.myChangeHandler}
+          onChange={(e)=> obj.problem = e.target.value}
         />
         <p>Enter details of any previous history of related problems:</p>
         <input
           type='text'
           name='previous'
-          onChange={this.myChangeHandler}
+          onChange={(e)=> obj.previous = e.target.value}
         />
         
         </form>
+//         <Form>
+//   <Form.Group controlId="formBasicEmail">
+//     <Form.Label>Email address</Form.Label>
+//     <Form.Control type="email" placeholder="Enter email" />
+//     <Form.Text className="text-muted">
+//       We'll never share your email with anyone else.
+//     </Form.Text>
+//   </Form.Group>
+
+//   <Form.Group controlId="formBasicPassword">
+//     <Form.Label>Password</Form.Label>
+//     <Form.Control type="password" placeholder="Password" />
+//   </Form.Group>
+//   <Form.Group controlId="formBasicCheckbox">
+//     <Form.Check type="checkbox" label="Check me out" />
+//   </Form.Group>
+//   <Button variant="primary" type="submit">
+//     Submit
+//   </Button>
+// </Form>
 
         //take problem, past, diagnosis etc
         );
