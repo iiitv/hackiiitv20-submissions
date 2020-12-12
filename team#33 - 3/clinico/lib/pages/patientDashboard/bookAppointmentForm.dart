207b34c51f29fa3d61605194e6fa87c5f14af6ad
Keshav Agarwal <keshavagarwal1710@gmail.com>
2020-12-12 13:27:57 +0530

    appointment form complete with backend

--- team#33 - 3/clinico/lib/pages/patientDashboard/bookAppointmentForm.dart
+++ team#33 - 3/clinico/lib/pages/patientDashboard/bookAppointmentForm.dart
@@ -1,6 +1,11 @@
 import 'dart:io';
 
+import 'package:clinico/model/appointment.dart';
 import 'package:clinico/model/user.dart';
+import 'package:clinico/pages/patientDashboard/notification.dart';
+import 'package:clinico/pages/role.dart';
+import 'package:clinico/services/backend.dart';
+import 'package:clinico/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -13,6 +18,11 @@ class BookAppointment extends StatefulWidget {
 }
 
 class _BookAppointmentState extends State<BookAppointment> {
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController ageController = new TextEditingController();
+  TextEditingController genderController = new TextEditingController();
+  TextEditingController commentController = new TextEditingController();
+  bool validName=true,validAge = true,validGender = true,isLoading = false;
 
   File image;
   final picker = ImagePicker();
@@ -25,11 +35,37 @@ class _BookAppointmentState extends State<BookAppointment> {
     });
   }
 
+  void bookAppointment()async{
+    String name = nameController.text.trim();
+    String age = ageController.text.trim();
+    String gender = genderController.text.trim();
+    String comment = commentController.text.trim();
+    setState((){
+      validName = name.isNotEmpty;
+      validAge = age.isNotEmpty;
+      validGender = gender.isNotEmpty;
+    });
+    if(validName && validAge && validGender && image!=null){
+      setState(()=>isLoading = true);
+      Appointment appoit = Appointment(
+        name:name,
+        age:int.parse(age),
+        gender: gender,
+        comment: comment,
+        image: image
+      );
+      await Backend().bookAppointment(appoit,widget.doctor.uid,currentUser.uid);
+      Navigator.pop(context);
+      Navigator.pop(context);
+      Navigator.push(context,MaterialPageRoute(builder:(BuildContext context)=>ShowNotification()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(title:Text("Book Appointment")),
-      body: Padding(
+      body: isLoading?Loading():Padding(
         padding: const EdgeInsets.all(13.0),
         child: ListView(
           children:[
@@ -38,7 +74,7 @@ class _BookAppointmentState extends State<BookAppointment> {
             ),
             SizedBox(height: 40),
             TextFormField(
-              // controller: educationalQualificationController,
+              controller: nameController,
               keyboardType: TextInputType.multiline,
               maxLines: null,
               decoration: InputDecoration(
@@ -47,11 +83,12 @@ class _BookAppointmentState extends State<BookAppointment> {
                 hintText: "Enter Patient Name",
                 border: OutlineInputBorder(),
                 labelText: "Name",
+                errorText: validName?null:"Name Can't Be Empty"
               ),
             ),
             SizedBox(height: 40),
             TextFormField(
-              // controller: educationalQualificationController,
+              controller: ageController,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 filled: true,
@@ -59,22 +96,24 @@ class _BookAppointmentState extends State<BookAppointment> {
                 hintText: "Enter Patient Age",
                 border: OutlineInputBorder(),
                 labelText: "Age",
+                errorText: validAge?null:"Age Can't Be Empty"
               ),
             ),
             SizedBox(height: 40),
             TextFormField(
-              // controller: educationalQualificationController,
+              controller: genderController,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.grey[50],
                 hintText: "Enter Patient Gender",
                 border: OutlineInputBorder(),
                 labelText: "Gender",
+                errorText: validGender?null:"Gender Can't Be Empty"
               ),
             ),
             SizedBox(height: 40),
             TextFormField(
-              // controller: educationalQualificationController,
+              controller: commentController,
               keyboardType: TextInputType.multiline,
               maxLines: null,
               decoration: InputDecoration(
@@ -92,7 +131,7 @@ class _BookAppointmentState extends State<BookAppointment> {
 
             ),
             (image==null)
-            ?Text("no file selected")
+            ?Text("no file selected(required)")
             :Hero(
                   tag: "heroImage",
                   child: AspectRatio(
@@ -107,6 +146,10 @@ class _BookAppointmentState extends State<BookAppointment> {
                       ),
                   )
               ),
+              RaisedButton(
+                child: Text("Submit"),
+                onPressed: (){bookAppointment();},
+              )
           ]
         ),
       ),
