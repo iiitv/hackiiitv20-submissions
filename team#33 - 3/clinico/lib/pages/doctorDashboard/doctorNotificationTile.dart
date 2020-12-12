f60b764eb0c48f028345f2c91e58b074fb12ae8b
Darshan Hande <201951052@iiitvadodara.ac.in>
2020-12-12 15:11:26 +0530

    doctor's appointment status frontend linked with backend

--- team#33 - 3/clinico/lib/pages/doctorDashboard/doctorNotificationTile.dart
+++ team#33 - 3/clinico/lib/pages/doctorDashboard/doctorNotificationTile.dart
@@ -5,21 +5,27 @@ import 'package:flutter/material.dart';
 class DocNotTile extends StatelessWidget {
   Appointment appointment;
   String msg;
-  DocNotTile({this.appointment}){
-    msg = appointment.confirmed?"You Confirmed ${appointment.name} Appointment":"${appointment.name} wants to book appointment.";
+  DocNotTile({this.appointment}) {
+    msg = appointment.confirmed
+        ? "You Confirmed ${appointment.name} Appointment"
+        : "${appointment.name} wants to book appointment.";
   }
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => ConfirmAppointment()));
-      },
-      child: Card(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ConfirmAppointment(
+                        appointment: appointment,
+                        msg: msg,
+                      )));
+        },
+        child: Card(
           child: ListTile(
             title: Text(msg),
           ),
-        )
-    );
+        ));
   }
 }
