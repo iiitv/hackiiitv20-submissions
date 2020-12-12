a29dfaa105d57590e77a63b7e6d4c792559551e4
Ashish Singh <ashish.k.singh1001@gmail.com>
2020-12-12 11:49:34 +0530

    Added Drug Guide

--- team#21-{3}/js/main.js
+++ team#21-{3}/js/main.js
@@ -1,63 +1,103 @@
-var symid, gender, dob, i=0;
+var symid, gender, dob, i = 0;
 
 var authtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1lY2lyMjczMDJAeWVrdGFyYS5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU2NjkiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIxMDkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiMTAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiQmFzaWMiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIwLTEyLTExIiwiaXNzIjoiaHR0cHM6Ly9hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjA3NzI4Njg5LCJuYmYiOjE2MDc3MjE0ODl9.w62c2YvV49Suor_0xIzi2h5tVF8rFZk89CttEdtCH2E";
 
 const api1 = `https://healthservice.priaid.ch/symptoms?token=${authtoken}&format=json&language=en-gb`;
 fetch(api1)
-        .then(response =>{
-            return response.json();
-        })
-        .then(data=>{
-            symdata = data;
-            //console.log(data);
-            
-});
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        symdata = data;
+        //console.log(data);
 
-function find(){
+    });
+
+function find() {
     symt = document.getElementById('sym').value;
     // gender = document.getElementById('gen').value;
     dob = document.getElementById('age').value;
 
-    if(document.getElementById('malRad').checked) { 
+    if (document.getElementById('malRad').checked) {
         gender = "Male";
-    } 
-    else if(document.getElementById('femRad').checked) { 
+    }
+    else if (document.getElementById('femRad').checked) {
         gender = "Female";
-    } 
+    }
 
-    for(i=0; i<281; i++){
-        if(symt.localeCompare(symdata[i].Name)){
+    for (i = 0; i < 281; i++) {
+        if (symt.localeCompare(symdata[i].Name)) {
             symid = symdata[i].ID;
         }
     }
 
     const api2 = `https://healthservice.priaid.ch/diagnosis?symptoms=[${symid}]&gender=${gender}&year_of_birth=${dob}&token=${authtoken}&format=json&language=en-gb`;
     fetch(api2)
-        .then(response =>{
+        .then(response => {
             return response.json();
         })
-        .then(data=>{ 
+        .then(data => {
             console.log(data);
 
-            document.getElementById('soln').innerHTML = data.map(sol => 
+            document.getElementById('soln').innerHTML = data.map(sol =>
                 `<div class="news">
                 
-                    <h2 id="icdname">
+                    <h2 class="heading-secondary no-bg" id="icdname">
                         ${sol.Issue.IcdName}
                     </h2>
-                    <h3 id="solname">
+                    <p class="paragraph" id="solname">
                         ${sol.Issue.Name}
-                    </h3>
+                    </p>
                     <br>
-                    <h2>Solution</h2>
+                    <h2 class="heading-secondary">Solution</h2>
                     ${sol.Specialisation.map(spe =>
-                        `<li>${spe.Name}</li>`
-                        ).join('')}
+                    `<li>${spe.Name}</li>`
+                ).join('')}
                 
                 <br><br>
                 <hr>
                 <br>
                 </div>`
             ).join('')
-    });
+        });
+}
+
+function run() {
+    drug = document.getElementById('drugname').value;
+
+    const api1 = `https://api.fda.gov/drug/label.json?search=${drug}`;
+    fetch(api1)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            var res1 = data.results[0].information_for_patients[0].split(".", 4);
+            var res2 = data.results[0].adverse_reactions[0].split(".", 3);
+
+            document.getElementById('soln').innerHTML =
+                `<div class="medData">
+                        <h2 class="heading-secondary no-bg" >${drug}</h2>
+                        <br><br>
+                        <h3 class="fhide heading-tertiary">Patient Advice</h3>
+                        <p class="thide paragraph">${data.results[0].information_for_patients[0]}</p>
+                        <br>
+                        <h3 class="fhide heading-tertiary">Adverse Reaction</h3>
+                        <p class="thide paragraph">${data.results[0].adverse_reactions[0]}</p>                                            
+                        <br>
+                        <h3 class="fhide heading-tertiary">Overdosage</h3>
+                        <p class="thide paragraph">${data.results[0].overdosage[0]}</p>                                            
+                        <br>
+                        <h3 class="fhide heading-tertiary">Pregnancy</h3>
+                        <p class="thide paragraph">${data.results[0].pregnancy[0]}</p>                                            
+                        <br>
+                        <h3 class="fhide heading-tertiary">Elements</h3>
+                        <p class="thide paragraph">${data.results[0].spl_product_data_elements[0]}</p>                                            
+                        <br>
+                    <br><br>
+                    <hr>
+                    <br>
+                    </div>`
+
+        });
 }
