17a6d3a34ff4f5a2bfed43e1e6e9f712170a8e89
Darshan Hande <201951052@iiitvadodara.ac.in>
2020-12-12 11:18:03 +0530

    added payment method field in hopital info

--- team#33 - 3/clinico/lib/pages/patientDashboard/hospitalProfile.dart
+++ team#33 - 3/clinico/lib/pages/patientDashboard/hospitalProfile.dart
@@ -55,124 +55,141 @@ class _HospitalProfileState extends State<HospitalProfile> {
                 title: Text(doctor.clinicName),
                 backgroundColor: Colors.indigo,
               ),
-              body: Container(
-                child: ListView(
-                  children: [
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Center(
-                      child: Container(
-                        margin: EdgeInsets.only(top: 20),
-                        child: CircleAvatar(
-                          backgroundImage: NetworkImage(doctor.photoURL),
-                          radius: 60,
-                        ),
+              body: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: Container(
+                  child: ListView(
+                    children: [
+                      SizedBox(
+                        height: 40,
                       ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Center(
-                      child: Text(
-                        doctor.clinicName,
-                        style: TextStyle(
-                          fontSize: 35,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Divider(),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        'Dr. ' + doctor.displayName,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        doctor.educationalQualification,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        doctor.bio,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        'Address : ' + doctor.address,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        'Timing : ' + doctor.timing,
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                      child: Text(
-                        'Fees : ' + doctor.fee + ' rs. at clinic',
-                        style: TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 70,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
-                      child: FlatButton(
-                        color: Colors.indigoAccent,
-                        child: Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            'Book Appointment',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                            ),
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(doctor.photoURL),
+                            radius: 60,
                           ),
                         ),
-                        onPressed: () {},
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Center(
+                        child: Text(
+                          doctor.clinicName,
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Divider(),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Dr. ' + doctor.displayName,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          doctor.educationalQualification,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          doctor.bio,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Address : ' + doctor.address,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Timing : ' + doctor.timing,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Fees : ' + doctor.fee + ' rs. at clinic',
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'Payment Method : ' + doctor.paymentMethod,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+                        child: FlatButton(
+                          color: Colors.indigoAccent,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              'Book Appointment',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
